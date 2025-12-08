f7681c0ba8d56bf9f574dc94cc920658f288db1d
nicubalica <nbalica@myges.fr>
2025-12-08 10:20:41 +0100

    feat(contact): add index() method to display contact form page

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,4 +7,12 @@ use Illuminate\Http\Request;
 class ContactController extends Controller
 {
 
+    /**
+     * Afficher le formulaire de contact.
+     */
+    public function index()
+    {
+        return view('contact');
+    }
+
 }
