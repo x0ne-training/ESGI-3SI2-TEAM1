20b36cdb9febe25ed6a9a7ad7aa69a25f55a9fea
Nicu BALICA <nicu.balica@monnoyeur.com>
2025-12-18 16:20:58 +0100

    fix(react): remove default homepage rendering

--- resources/js/App.tsx
+++ resources/js/App.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import "@/css/app.css";
-import Banner from "@/components/Banner";
 
 const App = () => {
-    return (
-        <div className="mx-auto flex max-w-5xl flex-col gap-6 px-4 py-8">
-            <Banner />
-            <div className="text-lg font-semibold text-slate-800">Hello From React</div>
-        </div>
-    );
+    return null;
 };
 
 export default App;
